dec300b4152754a4e2cd551ec15f8c9dc600e219
unknown <chammouma.marwan@gmail.com>
2023-07-18 12:53:26 +0200

    Added function in Grille.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.Grille import Grille
+
+system = Grille(16)
+print(system.lattice)
+print(system.average_magnetization())
--- src/Grille.py
+++ src/Grille.py
@@ -2,5 +2,19 @@ import numpy as np
 from numpy import random
 
 class Grille:
-    def __init__(taille):
-        np.random.choice([-1,1],size=(taille,taille))
+    def __init__(self, taille):
+        self.taille = taille
+        self.lattice = np.random.choice([-1,1],size=(taille,taille))
+
+    def energy(i,j):
+        size = Grille.shape[0]
+        left = Grille[i, (j - 1) ]
+        right = Grille[i, (j + 1) ]
+        up = Grille[(i - 1) , j]
+        down = Grille[(i + 1), j]
+        energy = - Grille[i, j] * (left + right + up + down)
+        return energy
+    
+    def average_magnetization(self):
+        mag = np.sum(self.lattice) / self.taille
+        return mag
