cab79b8b932c0489c05626df34c5b5aa652e4b0a
unknown <chammouma.marwan@gmail.com>
2023-07-18 13:19:47 +0200

    Add metropolis algorithm

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from src.Grille import Grille
 
-system = Grille(16)
+system = Grille(4)
+print(system.lattice)
+print(system.average_magnetization())
+
+system.metropolis(1, 0, 0)
+
 print(system.lattice)
 print(system.average_magnetization())
--- src/Grille.py
+++ src/Grille.py
@@ -6,10 +6,19 @@ class Grille:
         self.taille = taille
         self.lattice = np.random.choice([-1,1],size=(taille,taille))
 
-    def energy(i,j):
-        energy = - Grille[i, j] * (Grille[i, (j - 1) ] + Grille[i, (j + 1) ] + Grille[(i - 1) , j] + Grille[(i + 1), j])
+    def energy(self, i,j):
+        energy = - self.lattice[i, j] * (self.lattice[i, (j - 1) ] + self.lattice[i, (j + 1) ] + self.lattice[(i - 1) , j] + self.lattice[(i + 1), j])
         return energy
     
     def average_magnetization(self):
-        mag = np.sum(self.lattice) / self.taille
+        mag = np.sum(self.lattice) / self.taille**2
         return mag
+    
+    def metropolis(self, temperature, i,j):
+        deltaE = -2 * self.energy(i,j)
+
+        if deltaE < 0.0:
+            self.lattice[i,j] *= -1
+        elif np.random.random() < np.exp( - deltaE / temperature):
+            self.lattice[i,j] *= -1
+
