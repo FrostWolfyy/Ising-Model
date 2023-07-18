9a53ae935acf19af739f3c756295080138868fa6
unknown <chammouma.marwan@gmail.com>
2023-07-18 15:19:05 +0200

    Add First graphs

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from src.Grille import Grille
 from src.Ising import Ising
+import matplotlib.pyplot as plt
 
-system = Grille(16)
+system = Grille(10)
 
-test = Ising(system,100, 0.01)
+test = Ising(system,10000, 0.001)
 
 mag,en = test.run()
 
-print(mag)
-print("          ")
-print(en)
+#
+# Graphs
+#
+
+plt.figure(figsize=(10, 4))
+
+plt.subplot(1, 2, 1)
+plt.plot(mag)
+plt.xlabel('Iteration')
+plt.ylabel('Magnetization')
+plt.title('2D Ising Model - Magnetization')
+
+plt.subplot(1, 2, 2)
+plt.plot(en)
+plt.xlabel('Iteration')
+plt.ylabel('Energy')
+plt.title('2D Ising Model - Energy')
+
+plt.tight_layout()
+plt.savefig("out/Mag_En_Iterations.pdf")
